e4541403bfa89766c51f367b7c9a21eccfeec564
tisyani <tisoczki.janos@diak.szbi-pg.hu>
2025-09-10 08:47:05 +0200

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,7 @@
+
+print ("enter your last name")
+lastname = input()
+print ("enter your last name")
+firstname = input()
+print ("hello, " + lastname + " " + firstname + ":3")   
+print  (f"hello, {lastname} {firstname} :3")
