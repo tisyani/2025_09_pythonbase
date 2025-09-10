525073872414d979fa981b0f5f6a2249a4d7d55f
tisyani <tisoczki.janos@diak.szbi-pg.hu>
2025-09-10 11:28:46 +0200

    sad

---
+++ 02_adattipusok.py
@@ -0,0 +1,5 @@
+name ="Tisóczki János"
+age = 17
+height = 1.91
+is_student = True   
+print(f"name: {name}, age: {age}, height: {height}, is student {is_student} ")
---
+++ feladatok/3_1.py
@@ -0,0 +1,8 @@
+'''
+Thonny fejlesztői környezetben készíts egy rövid programot, amely
+kommentként tartalmazza a program funkciójának leírását,
+konstansként tárolja PI értékét,
+egy másik változóban tárolja egy kör sugarának nagyságát (cm-ben megadva),
+kiszámolja és a képernyőre kiírja a kör kerületét és területét!
+'''
+
